df87bcbb7d83da45b824158332dbd026ffff1b3e
Yvan Martinez <yvangamboa@gmail.com>
2019-11-06 21:23:11 -0500

    Initial commit

---
+++ src/main/java/com/confluent/example/restkafkaavro/RestKafkaAvroApplication.java
@@ -0,0 +1,13 @@
+package com.confluent.example.restkafkaavro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKafkaAvroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestKafkaAvroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/confluent/example/restkafkaavro/RestKafkaAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confluent.example.restkafkaavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestKafkaAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
